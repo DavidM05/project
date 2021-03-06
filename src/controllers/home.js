861370b6c53fea54dbafb653d7a6a9b45453615e
DavidM05 <DavidM05@users.noreply.github.com>
2021-03-06 09:47:36 -0500

    .

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,17 +1,14 @@
 const ctrl = {};
 
 const { Image } = require('../model');
-const { ImgMarket } = require('../model');
+
 
 ctrl.index = async (req, res) =>{
     const images = await Image.find().sort({timestamp: -1});
     res.render('index', {images});
 };
 
-imgM.index = async (req, res) =>{
-    const imgM = await ImgMarket.fin().sort({timestamp: -1});
-    res.render('index', {imgM});
-}
 
 
-module.exports = ctrl, imgM;
+
+module.exports = ctrl;
